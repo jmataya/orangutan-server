4df1969c9c6ea8d10d412825fe898282a7581d2d
Jeff Mataya <jeff.mataya@gmail.com>
2012-10-19 15:32:51 -0700

    Initial framework for the server
    Just a basic commit of the RoR framework that will become this awesome
    web service!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OrangutanServer::Application.config.secret_token = 'c19a8e06b81785cda2873bb58c66eb202aa2f30a8a0ba604d4f035898f2a3691d0d4456c870e5e89d2db86ede7624bebdfc2df7d0158a5e09fc3180ae7b7bf38'
