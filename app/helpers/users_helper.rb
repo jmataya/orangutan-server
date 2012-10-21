c94c0ab97be330c768a5fe1392842be2e05f2505
Jeff Mataya <jeff.mataya@gmail.com>
2012-10-21 11:40:59 -0700

    Fixed the authorization module and JSON for user

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,2 @@
 module UsersHelper
-	def signed_in_user
-    auth_id = params[:auth]
-    user_id = params[:uid]
-    if auth_id.nil? or auth_id != "00000000-0000-0000-0000-000000000000" or user_id.nil?
-      redirect_to root_path
-    end
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,9 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 256 }
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
   		uniqueness: { case_sensitive: false }
+
+  def to_json(object)
+  	json_encoder = ActiveSupport::JSON
+  	json_encoder.encode({ id: self.id, name: self.name, email: self.email })
+  end
 end
