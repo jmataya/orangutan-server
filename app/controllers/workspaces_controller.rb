971ef59e43820dae5355833fa97381e0074c972d
Jeff Mataya <jeff.mataya@gmail.com>
2012-10-22 14:57:51 -0700

    Simplified JSON handling in WorkspacesController

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -1,73 +1,45 @@
 class WorkspacesController < ApplicationController
 	before_filter :signed_in_user
+
+	respond_to :json
 	
 	def index
 		@user = User.find(params[:user_id])
-		@workspaces = @user.workspaces
-
-		respond_to do |format|
-			format.json { render json: @workspaces }
-		end
+		respond_with @user.workspaces
 	end
 
 	def show
 		@user = User.find(params[:user_id])
-		@workspace = @user.workspaces.find_by_id(params[:id])
-
-		respond_to do |format|
-			format.json { render json: @workspace }
-		end
-	end
-
-	def new
-		@workspace = Workspace.new
-
-		respond_to do |format|
-			format.json { render json: @workspace }
-		end
-	end
-
-	def edit
-		@user = User.find(params[:user_id])
-		@workspace = @user.workspaces.find_by_id(params[:id])
+		respond_with @user.workspaces.find_by_id(params[:id])
 	end
 
 	def create
 		@user = User.find_by_id(params[:user_id])
 		@workspace = Workspace.new(params[:workspace])
 
-		respond_to do |format|
-			if @workspace.save
-				@uwr = @workspace.user_workspace_relationships.build(user_id: @user.id)
-				if @uwr.save
-					format.json { render json: @workspace, status: :created, location: @user }
-				else
-					format.json { render json: @uwr.errors, status: :unprocessable_entity }
-				end
+		if @workspace.save
+			@uwr = @workspace.user_workspace_relationships.build(user_id: @user.id)
+			if @uwr.save
+				respond_with @workspace
 			else
-				format.json { render json: @workspace.errors, status: :unprocessable_entity }
+				respond_with @uwr
 			end
+		else
+			respond_with @workspace
 		end
 	end
 
 	def update
 		@workspace = Workspace.find(params[:id])
-
-		respond_to do |format|
-			if @workspace.update_attributes(params[:workspace])
-				format.json { head :no_content }
-			else
-				format.json { render json: @workspace.errors, status: :unprocessable_entity }
-			end
-		end
+		@workspace.update_attributes(params[:workspace])
+		respond_with @workspace
 	end
 
 	def destroy
 		@workspace = Workspace.find(params[:id])
 		@workspace.destroy
 
-		respond_to do |format|
-			format.json { head :no_content }
-		end
+		@message = "Workplace successfully destroyed."
+		respond_with @message
 	end
 end
